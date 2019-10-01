9dd5fe39e9bd54b0485054767a16042641dbd5b1
Gimaevazat <55442406+Gimaevazat@users.noreply.github.com>
2019-10-01 17:08:16 +0500

    Обе ракетки движутся, шарик отскакивает от стен и ракеток

--- tennis.py
+++ tennis.py
@@ -7,33 +7,45 @@ def Moveracket2(event):
     if x2 < 340:
         if event.keycode == VK_RIGHT:
             x2 += Vracket
-            moveObjectTo(racket2, x2, y2)
     if x2 > 2:
         if event.keycode == VK_LEFT:
             x2 -= Vracket
-            moveObjectTo(racket2, x2, y2)
+    moveObjectTo(racket2, x2, y2)
 
 def Moveracket1():
     global x1, y1, racket1
+    if x1 < 340 and x1 < xball:
+        x1 += Vracket
+    if x1 > 2 and x1 + 60 > xball:
+        x1 -= Vracket
+    moveObjectTo(racket1, x1, y1)
 
 
 def Moveball():
-    global Vxball, Vyball, xball, yball
+    global Vxball, Vyball, xball, yball, ball
+    if xball > 385:
+        Vxball = - Vxball
+    if xball < 2:
+        Vxball =  -Vxball
+    if yball > y2 - 15 and xball > x2 - 8  and xball < x2 + 54:
+        Vyball = -Vyball
+    if yball < y1 + 9 and xball > x1 - 8  and xball < x2 + 54:
+        Vyball = -Vyball
     xball += Vxball
     yball += Vyball
     moveObjectTo(ball, xball, yball)
 
 def wall():
-    pass
+    global Vxball, xball
 
-Vxball = 1
-Vyball  = 1
+Vxball = 5
+Vyball  = 5
 Vracket = 7
 x2 = 20
 y2 = 563
 x1 = 20
 y1 = 30
-xball = 200
+xball = 20
 yball = 300
 
 penSize(0)
@@ -50,6 +62,7 @@ brushColor(randColor())
 ball = circle(xball, yball, 7)
 
 onTimer(Moveball, 50)
+onTimer(Moveracket1, 50)
 onKey(Moveracket2)
 
 run()
