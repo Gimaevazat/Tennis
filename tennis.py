303dc17d241fd76f60c797edfe0e26e6cd3a1dc0
Gimaevazat <55442406+Gimaevazat@users.noreply.github.com>
2019-10-01 16:32:34 +0500

    Написаны функции движения шарика, и одной ракетки

--- tennis.py
+++ tennis.py
@@ -1,28 +1,40 @@
+from tkinter import *
 from graph import *
 import random
 
-def Key(event):
-    pass
-    #обработчик удержания клавиш влево, вправо, при которых ракетка 2 будет двигаться вправо, влево
+def Moveracket2(event):
+    global x2, y2, racket2, Vracket
+    if x2 < 340:
+        if event.keycode == VK_RIGHT:
+            x2 += Vracket
+            moveObjectTo(racket2, x2, y2)
+    if x2 > 2:
+        if event.keycode == VK_LEFT:
+            x2 -= Vracket
+            moveObjectTo(racket2, x2, y2)
 
-def Mover1():
-    pass
-    #движение ракетки 1, ей управляет комп
+def Moveracket1():
+    global x1, y1, racket1
 
-def Mover2():
-    pass
-    #движение ракетки 2, управляет человвек
 
 def Moveball():
+    global Vxball, Vyball, xball, yball
+    xball += Vxball
+    yball += Vyball
+    moveObjectTo(ball, xball, yball)
+
+def wall():
     pass
-    #движения шарика с ударом об стены и ракетки
 
-
-
-Vball = 0
-Vracket = 0
-
-
+Vxball = 1
+Vyball  = 1
+Vracket = 7
+x2 = 20
+y2 = 563
+x1 = 20
+y1 = 30
+xball = 200
+yball = 300
 
 penSize(0)
 brushColor(randColor())
@@ -30,11 +42,14 @@ rectangle(1, 1, 401, 601)
 windowSize(402, 602)
 
 brushColor(randColor())
-racket1 = rectangle(20, 30, 80, 37)
-racket2 = rectangle(20, 563, 80, 570)
+racket1 = rectangle(x1, y1, x1 + 60, y1 + 7)
+racket2 = rectangle(x2, y2, x2 + 60, y2 + 7 )
 
 
 brushColor(randColor())
-ball = circle(200, 300, 5)
+ball = circle(xball, yball, 7)
+
+onTimer(Moveball, 50)
+onKey(Moveracket2)
 
 run()
