3a79499236fcec12a37085d4463670c7afc11a19
Korzun Dmitriy <Korzun.DV@phystech.edu>
2019-10-08 11:14:32 +0300

    Дизайн

--- tennis.py
+++ tennis.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from graph import *
 import random
-
+Score = 0
+#Ракетка игрока
 def Moveracket2(event):
     global x2, y2, racket2, Vracket
     if x2 < 340:
@@ -11,7 +12,7 @@ def Moveracket2(event):
         if event.keycode == VK_LEFT:
             x2 -= Vracket
     moveObjectTo(racket2, x2, y2)
-
+#Ракетка противника
 def Moveracket1():
     global x1, y1, racket1
     if x1 < 340 and x1 < xball:
@@ -20,7 +21,7 @@ def Moveracket1():
         x1 -= Vracket
     moveObjectTo(racket1, x1, y1)
 
-
+# Теннисный мяч
 def Moveball():
     global Vxball, Vyball, xball, yball, ball
     if xball > 385:
@@ -31,6 +32,10 @@ def Moveball():
         Vyball = -Vyball
     if yball < y1 + 9 and xball > x1 - 8  and xball < x2 + 54:
         Vyball = -Vyball
+    if yball < 555:
+        Score =+ 1
+    if Score == 5:
+        end()
     xball += Vxball
     yball += Vyball
     moveObjectTo(ball, xball, yball)
@@ -40,7 +45,7 @@ def end():
     # конец игры
 
 Vxball = 5
-Vyball  = 5
+Vyball  = 7
 Vracket = 7
 x2 = 20
 y2 = 563
@@ -48,18 +53,19 @@ x1 = 20
 y1 = 30
 xball = 20
 yball = 300
-
+#Дизфйн стола
 penSize(0)
-brushColor(randColor())
+brushColor("green")
 rectangle(1, 1, 401, 601)
 windowSize(402, 602)
-
-brushColor(randColor())
+#Дизайн ракеток
+brushColor("red")
 racket1 = rectangle(x1, y1, x1 + 60, y1 + 7)
+brushColor("blue")
 racket2 = rectangle(x2, y2, x2 + 60, y2 + 7 )
 
-
-brushColor(randColor())
+#Дизайн мяча
+brushColor("orange")
 ball = circle(xball, yball, 7)
 
 onTimer(Moveball, 50)
